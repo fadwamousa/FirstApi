c71d6b091bd2483cac1d3a3a8d4a9963c7226c97
fadwa <fadwaana99@gmail.com>
2019-03-30 12:50:33 +0200

    First Commit

---
+++ routes/api.php
@@ -0,0 +1,19 @@
+<?php
+
+use Illuminate\Http\Request;
+
+
+
+Route::middleware('auth:api')->get('/user', function (Request $request) {
+    return $request->user();
+});
+
+Route::apiResource('products','ProductController');
+
+Route::group(['prefix'=>'products'],function(){
+
+	Route::apiResource('{product}/reviews','ReviewController');
+
+});
+//localhost/FirstApi_product/products
+//localhost/FirstApi_product/products/12/reviews
